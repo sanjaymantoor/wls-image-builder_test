a252589adeef95c512a7fb1b253b4978e3bcf3a5
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2021-11-11 12:02:23 +0530

    Added some more debug lines

--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
+echo "Starting test wls setup"
+pwd
+cd /var/lib/waagent
+mkdir wlssetup
+cd wlssetup
 pwd
 export WEBLOGIC_DEPLOY_TOOL=https://github.com/oracle/weblogic-deploy-tooling/releases/download/weblogic-deploy-tooling-1.8.1/weblogic-deploy.zip
 
 wget $WEBLOGIC_DEPLOY_TOOL
+
+echo "Ending test wls setup"
