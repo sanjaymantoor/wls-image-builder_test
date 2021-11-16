b4175ba67164a867b15e6129244902d265cd61d1
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2021-11-16 14:45:36 +0530

    Testing setup WLS

--- setupWLS.sh
+++ setupWLS.sh
@@ -9,7 +9,7 @@ function echo_stderr ()
 #Function to display usage message
 function usage()
 {
-  echo_stderr "./installWeblogic.sh <<< <parameters>"
+  echo_stderr "./setupWLS.sh <<< <parameters>"
 }
 
 
