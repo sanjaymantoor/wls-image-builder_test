fee8752e905a2adce6f8bc141746e6df7e69fc83
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2021-11-24 14:01:56 +0530

    Testing without reboot

--- setupWLS.sh
+++ setupWLS.sh
@@ -458,11 +458,19 @@ echo "================================================================="
 osVersion=`cat /etc/os-release | grep VERSION_ID |cut -f2 -d"="| sed 's/\"//g'`
 majorVersion=`echo $osVersion |cut -f1 -d"."`
 minorVersion=`echo $osVersion |cut -f2 -d"."`
+
+echo "Kernel version before update:"
+uname -a
+
 echo yum upgrade -y --disablerepo=ol7_latest  --enablerepo=ol${majorVersion}_u${minorVersion}_base
 yum upgrade -y --disablerepo=ol7_latest  --enablerepo=ol${majorVersion}_u${minorVersion}_base
+
+echo "Kernel version after update:"
+uname -a
 
 echo "================================================================="
 echo "##########           Update software completed         ##########"
 echo "=================================================================" 
+
 
 
