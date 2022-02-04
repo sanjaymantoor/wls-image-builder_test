974d15ace4281ab9e25212d7fd5482fd9e702155
Sanjay Mantoor <sanjay.mantoor@oracle.com>
2022-02-04 12:23:35 +0530

    Added testclusteroffer script

---
+++ testClusterOffer.sh
@@ -0,0 +1,17 @@
+# This script test the base VM with provided inputs
+
+read testPropertyFile
+
+yum install -y git jq
+mkdir -p /root/scripts
+cd /root/scripts
+git clone https://github.com/gnsuryan/azure-wls-test
+
+cd /root/scripts/azure-wls-test
+
+chmod +x ./basic/basic_test.sh
+
+./cluster/cluster_test.sh -i /root/scripts/azure-wls-test/test_input/${testPropertyFile} 
+
+
+
